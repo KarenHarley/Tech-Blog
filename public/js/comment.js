bfa74e8aa45c6fb6362185ae3d40de9d2fdb1f7c
Karen V <karen.pretzel.365@gmail.com>
2021-10-07 17:36:58 -0500

    some work

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,10 +1,15 @@
-
-
 const comment = document.querySelector("#commentText").value;
-console.log("Js script is linked!")
+const button = document.querySelector("#postBtn")
+console.log("Js script is linked!");
+
 const handleCommentSubmit = (event) => {
   event.preventDefault(); //you're supposed to receive an event object here.
-  
-//  console.log(comment + id);
-  console.log(event.target.dataset.postId) //let's try this instead
+
+  //  console.log(comment + id);
+  console.log(event.target.dataset.postId); //let's try this instead
+  console.log("Hi i work");
+  console.log(event);
+  console.log("You clicked ", event.target);
 };
+
+button.addEventListener("click", handleCommentSubmit)
